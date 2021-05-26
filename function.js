ed01e0c1a230ceaabf6474aae7475adce13c3f9f
www-data <www-data@ip-172-31-2-100.ap-northeast-1.compute.internal>
2021-05-26 07:15:32 +0000

    fix URL in function.js

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
-url_urltowords = "http://urltowords.kajindowsxp.com/backend/urltowords.php"
-url_wordstourl = "http://urltowords.kajindowsxp.com/backend/wordstourl.php"
+url_urltowords = "https://urltowords.com/backend/urltowords.php"
+url_wordstourl = "https://urltowords.com/backend/wordstourl.php"
 function texttowords(text) {
     text = encodeURIComponent(text);
     var request = createXmlHttpRequest();
