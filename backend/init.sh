a2ee610dcc497d34bee17c1e0ebbb48c4c8ab78e
kajikentaro <k.kentarou0412@gmail.com>
2021-09-02 07:39:20 +0000

    fix init.sh

--- backend/init.sh
+++ backend/init.sh
@@ -1,3 +1,3 @@
 sqlite3 urltowords.db "CREATE TABLE urltowords (id integer primary key autoincrement, category text,value text, word1 text, word2 text, word3 text, date date default CURRENT_TIMESTAMP);"
 sqlite3 urltowords.db "CREATE TABLE words (id int,word text);"
-sqlite3 -separator , urltowords.db ".import words800.csv words"
+sqlite3 -separator , urltowords.db ".import backend/words800.csv words"
