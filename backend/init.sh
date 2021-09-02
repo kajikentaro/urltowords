81befe7cd31622a1f9773a620c34377a74b7cb34
kajikentaro <k.kentarou0412@gmail.com>
2021-09-02 13:07:16 +0000

    fix end-of-line code to LF

--- backend/init.sh
+++ backend/init.sh
@@ -1,4 +1,5 @@
 sqlite3 urltowords.db "CREATE TABLE urltowords (id integer primary key autoincrement, category text,value text, word1 text, word2 text, word3 text, date date default CURRENT_TIMESTAMP);"
 sqlite3 urltowords.db "CREATE TABLE words (id int,word text);"
 sqlite3 -separator , urltowords.db ".import words800.csv words"
-busybox chmod 777 urltowords.db
+chmod 777 urltowords.db
+chmod 777 $(pwd)
