db1cc5a65c67ac5a08642bcf1cb9046fc965f049
kajikentaro <58505538+kajikentaro@users.noreply.github.com>
2021-09-02 16:49:06 +0900

    update init.sh

--- backend/init.sh
+++ backend/init.sh
@@ -1,4 +1,3 @@
-cd `dirname $0`
 sqlite3 urltowords.db "CREATE TABLE urltowords (id integer primary key autoincrement, category text,value text, word1 text, word2 text, word3 text, date date default CURRENT_TIMESTAMP);"
 sqlite3 urltowords.db "CREATE TABLE words (id int,word text);"
 sqlite3 -separator , urltowords.db ".import words800.csv words"
